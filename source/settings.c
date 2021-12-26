ce8e315bfc591ee94c5221db1c47e5b29dd4dc0b
SecretPocketCat <jiridexterhanc@gmail.com>
2021-12-26 15:46:52 +0100

    Removed unused profiles

--- source/settings.c
+++ source/settings.c
@@ -6,22 +6,8 @@
  * Add profiles from source/profiles.h in the profile array
  */
 profile profiles[] = {
-    /* {colorBleed, {0, 0, 0, 0}, NULL, NULL}, */
-    {white, {0, 0, 0, 0}, NULL, NULL},
-    {red, {0, 0, 0, 0}, NULL, NULL},
-    {green, {0, 0, 0, 0}, NULL, NULL},
-    {blue, {0, 0, 0, 0}, NULL, NULL},
-    {rainbowHorizontal, {0, 0, 0, 0}, NULL, NULL},
-    {rainbowVertical, {0, 0, 0, 0}, NULL, NULL},
-    {animatedRainbowVertical, {35, 28, 21, 14}, NULL, NULL},
-    {animatedRainbowFlow, {7, 5, 2, 1}, NULL, NULL},
-    {animatedRainbowWaterfall, {7, 5, 2, 1}, NULL, NULL},
-    {animatedBreathing, {5, 3, 2, 1}, NULL, NULL},
-    {animatedWave, {5, 3, 2, 1}, NULL, NULL},
-    {animatedSpectrum, {11, 6, 4, 1}, NULL, NULL},
-    {reactiveFade, {4, 3, 2, 1}, reactiveFadeKeypress, reactiveFadeInit},
-    {reactivePulse, {4, 3, 2, 1}, reactivePulseKeypress, reactivePulseInit},
-    {reactiveTerm, {1, 2, 3, 4}, reactiveTermKeypress, reactiveTermInit}};
+    {reactiveFade, {4, 3, 2, 1}, reactiveFadeKeypress, reactiveFadeInit}
+};
 
 /* Set your defaults here */
 uint8_t currentProfile = 0;
